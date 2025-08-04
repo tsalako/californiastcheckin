7133348882dece4bcfb230c02b0ee5b5d837ee5c
Tofe Salako <tsalako12@gmail.com>
2025-08-04 00:38:39 -0700

    include body parser

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("index", {
